176bf24f66761ab5ca06053a85415509f44dc725
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-03 13:08:03 +0100

    Added switch so only one route will be displayed. Fixed mispelling bug in about.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // Import pages
 import Home from "./pages/home";
@@ -13,16 +13,17 @@ function App() {
         <nav>
           <h1>My Articles</h1>
         </nav>
-
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About/>
-        </Route>
-        <Route path="/contact">
-          <Contact/>
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/contact">
+            <Contact/>
+          </Route>
+        </Switch>
 
       </BrowserRouter>
     </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-        <h2>Contact</h2>
+        <h2>About</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus explicabo quam necessitatibus officia, tenetur quia iusto quod dolorum nobis, a minima quaerat at expedita eos aliquam amet, minus veritatis deserunt.</p>
     </div>
   )
