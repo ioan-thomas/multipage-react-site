be62647cfaeb03f934196ad3847f4c4676d13930
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-03 12:12:13 +0100

    setup initial project pages

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <h2>Contact</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus explicabo quam necessitatibus officia, tenetur quia iusto quod dolorum nobis, a minima quaerat at expedita eos aliquam amet, minus veritatis deserunt.</p>
+    </div>
+  )
+}
