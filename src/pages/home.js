716ae29e849fdeacc45c4f9bb573dea03014a428
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-03 17:43:46 +0100

    Fetching the articles and outputting details to the homepage.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { useFetch } from '../hooks/useFetch'
+
+// styles
+import "./home.css"
 
 export default function Home() {
+
+  const { data: articles, isPending, error } = useFetch('http://localhost:3000/articles')
+
   return (
-    <div>
-        <h2>Homepage</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus explicabo quam necessitatibus officia, tenetur quia iusto quod dolorum nobis, a minima quaerat at expedita eos aliquam amet, minus veritatis deserunt.</p>
+    <div className='home'>
+        <h2>Articles</h2>
+        {isPending && <div>Loading...</div>}
+        {error && <div>{error}</div>}
+        {articles && articles.map( article => (
+          <div key={article.id} className="card">
+            <h3>{article.title}</h3>
+            <p>{article.author}</p>
+          </div>
+        ))}
+        
     </div>
   )
 }
